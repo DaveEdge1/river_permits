9e93132439c819a96236ff2dba842cb199060742
Dave Edge <david.edge@nau.edu>
2026-01-28 13:59:13 -0700

    app build

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -5,4 +5,7 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.4" apply false
+
 }
